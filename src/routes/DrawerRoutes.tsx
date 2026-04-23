4e3f263f95f9894eca91bed4e3fbfb18aed5fea6
nicolesuzuki <japadasuzuki@gmail.com>
2026-04-23 01:49:38 +0000

    22/04

--- src/routes/DrawerRoutes.tsx
+++ src/routes/DrawerRoutes.tsx
@@ -1,17 +1,67 @@
-import { createDrawerNavigator } from "@react-navigation/drawer";
+import { createDrawerNavigator, DrawerContentScrollView, DrawerItemList } from "@react-navigation/drawer";
 import HomeScreen from "../screens/HomeScreen";
 import EntidadeScreen from "../screens/EntidadeScreen";
 import ContasPagarScreen from "../screens/ContasPagarScreen";
 import ContasReceberScreen from "../screens/ContasReceberScreen";
+import { Ionicons } from "@expo/vector-icons";
+import{View, Text, Image, StyleSheet, TouchableOpacity} from "react-native";
 
 const Drawer = createDrawerNavigator();
-export default function DrawerRoutes() {
-    return (
-        <Drawer.Navigator>
-            <Drawer.Screen name="Home" component={HomeScreen}  />
-            <Drawer.Screen name="Entidades" component={EntidadeScreen}  />
-            <Drawer.Screen name="Contas a Pagar" component={ContasPagarScreen}  />
-            <Drawer.Screen name="Contas a Receber" component={ContasReceberScreen} />
-        </Drawer.Navigator>
+function CustomDrawerContent(props: any){
+    const user = {
+        nome:"João Silva",
+        foto: "https://i.pravatar.cc/100"
+    };
+    return(
+        <View style={styles.container}>
+            <DrawerContentScrollView {...props}>
+                <view style={styles.header}>
+                    <Image
+                    source={{uri:user.foto}}
+                    style={styles.avatar}
+                    />
+                    <Text style={styles.name}>
+                        {user.nome}
+                    </Text>
+                </view>
+                <DrawerItemList {...props}/>
+            </DrawerContentScrollView>
+            <View style={styles.footer}>
+                <TouchableOpacity
+                style={styles.logoutButton}
+                onPrees={() => console.log("logout")}>
+
+                <Ionicons name="log-out-outline" size={22} color="#fff" />
+                <Text style={styles.logoutText}>
+                        Deslogar
+                </Text>
+
+                </TouchableOpacity>
+            </View>
+        </View>
     );
+}
+export default function DrawerRoutes (){
+    return (
+        <Drawer.Navigator
+        drawerContent={(props) => <CustomDrawerContent {...props} />}
+        screenOptions={{
+            headerStyle: {
+                backgroundColor: "#1E88E5"
+            },
+            headerTintColor:"#fff",
+            drawerActiveTintColor:"#1E88E5"
+        }}>
+            <Drawer.Screen
+            name="Home"
+            component={HomeScreen}
+            options={{
+                drawerIcon: ({ color, size}) => (
+                    <Ionicons name="home-outline" size={size} color={color}/>
+                )
+            }}
+            />
+            
+        </Drawer.Navigator>
+    )
 }
